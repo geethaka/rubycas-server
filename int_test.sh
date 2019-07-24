1618aced035f08076a52c83e55e34d0863fb9317
Kasun Geethaka <geethakacdsdt@gmail.com>
2019-07-24 20:01:56 +0530

    Update int_test.sh

--- int_test.sh
+++ int_test.sh
@@ -3,7 +3,7 @@
 # added a sleep since kubernets deployment can take time to propogate
 sleep 60 
 
-test_result=$(curl -s -o /dev/null -w "%{http_code}" http://rubycas.k8.bebraven.org1/login)
+test_result=$(curl -s -o /dev/null -w "%{http_code}" http://rubycas.k8.bebraven.org/login)
 
 echo $test_result
 if [ $test_result == 200 ]
